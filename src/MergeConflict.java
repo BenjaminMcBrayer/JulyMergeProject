94e4a942b293119a1df3e63790cdabf21f7580d0
Antonella Solomon <antonellasolomon@gmail.com>
2018-08-02 11:39:56 -0400

    added some changes

--- src/MergeConflict.java
+++ src/MergeConflict.java
@@ -4,6 +4,8 @@ public class MergeConflict {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		System.out.println("My name is Ben.");
+		
+		System.out.println("Hello, I'm Antonella!");
 
 	}
 
